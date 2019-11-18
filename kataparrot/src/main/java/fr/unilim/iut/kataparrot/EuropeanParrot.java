bf553134fce2b7aa020df3c1590017b221bf7273
FredericCanaud <frederic.canaud@etu.unilim.fr>
2019-11-18 17:17:10 +0100

    Mise en place polymorphisme vitesse d'un perroquet européen

---
+++ kataparrot/src/main/java/fr/unilim/iut/kataparrot/EuropeanParrot.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.kataparrot;
+
+public class EuropeanParrot extends Parrot {
+	
+	public EuropeanParrot(ParrotTypeEnum _type, int numberOfCoconuts, double voltage, boolean isNailed) {
+		super(_type, numberOfCoconuts, voltage, isNailed);
+	}
+
+	@Override
+	public double getSpeed() {
+		return getBaseSpeed();
+	}
+}
